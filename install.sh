6ffabda4a97b00f0a56025de027575e7000e96fa
Hamid <hamidmamadzade@gmail.com>
2019-03-21 12:43:01 +0100

    install scripts (WIP)

--- install.sh
+++ install.sh
@@ -1,71 +1,149 @@
 #!/bin/sh
 
-echo '---------INSTALLATION-SCRIPT---------\n'
+cat << Output
+------------------------------------------------------------------------
+                                                     INSTALLATION-SCRIPT
+------------------------------------------------------------------------
 
-echo 'install'
+
+
+------------------------------------
+                             install
+------------------------------------
+Output
+
 if [[ "$(uname)" == "Darwin" ]]; then
-
-  # colorscheme setup
-  # echo 'install base16'
-  # git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
   # vim installation (mac comes pre-installed with vim but it is compiled without clipboard)
   # this step is not needed if you have a version of vim that has system clipboard functionality
   # you can check this by running `vim --version | grep clipboard` in your terminal
   # if you see `+clipboard` then you're fine
-  echo 'installing vim\n'
+cat << Output
+                                    ------------------------------------
+                                    installing vim
+                                    ------------------------------------
+Output
+
   brew install vim
 
   # fuzzy finder setup
-  echo 'installing fzf\n'
+cat << Output
+------------------------------------
+                      installing fzf
+------------------------------------
+Output
+
   brew install fzf
 
-  echo 'installing fzf fuzzy completion\n'
+cat << Output
+                                    ------------------------------------
+                                    installing fzf fuzzy completion
+                                    ------------------------------------
+Output
+
   # To install useful key bindings and fuzzy completion:
   $(brew --prefix)/opt/fzf/install
 
-  # asdf version manager. Supported languages include Ruby, Node.js, Elixir and more.
-  echo 'installing asdf, the extendable version manager\n'
+  # asdf, the extendable version manager.
+  # Supported languages include Ruby, Node.js, Elixir and more.
+cat << Output
+------------------------------------
+                     installing asdf
+------------------------------------
+Output
+
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.4.0
   echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc
   echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
 
   # tig: text-mode interface for Git
-  echo 'installing tig (text-mode interface for Git)'
+cat << Output
+                                    ------------------------------------
+                                    installing tig
+                                    ------------------------------------
+Output
+
   brew install tig
 
   # openssl: general purpose cryptographic library
-  echo 'installing openssl\n'
+cat << Output
+------------------------------------
+                  installing openssl
+------------------------------------
+Output
+
   brew install openssl
 
   # Programming language prerequisites and package managers
-  echo 'installing libyaml\n'
+cat << Output
+                                    ------------------------------------
+                                    installing libyaml
+                                    ------------------------------------
+Output
+
   brew install libyaml # should come after openssl
 
-  echo 'installing coreutils\n'
+  # basic file, shell and text manipulation utilities of the GNU operating system
+cat << Output
+------------------------------------
+                installing coreutils
+------------------------------------
+Output
+
   brew install coreutils
 
-  echo 'installing yarn\n'
+  # yarn: a new package manager for javaScript
+cat << Output
+                                    ------------------------------------
+                                    installing yarn
+                                    ------------------------------------
+Output
+
   brew install yarn
 
-  echo 'installing ImageMagick\n'
+  # create, edit, compose, or convert bitmap images
+cat << Output
+------------------------------------
+              installing ImageMagick
+------------------------------------
+Output
+
   brew install imagemagick
 
   # Use GPG Suite to encrypt, decrypt, sign and verify files or messages
-  echo 'installing GNUpg'
+cat << Output
+                                    ------------------------------------
+                                    installing GNUpg
+                                    ------------------------------------
+Output
+
   brew install gpg
 
   # MySQL database
-  echo 'installing MySQL'
+cat << Output
+------------------------------------
+                    installing MySQL
+------------------------------------
+Output
+
   brew install mysql
 
-  A code searching tool.
-  echo 'installing ripgrep'
+  # A code searching tool.
+cat << Output
+                                    ------------------------------------
+                                    installing ripgrep
+                                    ------------------------------------
+Output
+
   brew install ripgrep
 
-
   # colorscheme setup
-  echo 'base16'
+cat << Output
+------------------------------------
+                              base16
+------------------------------------
+Output
+
   git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
 fi
--- tmux/install.sh
+++ tmux/install.sh
@@ -1,66 +1,55 @@
 #!/bin/sh
 
-shell_message() {
-cat << Output
-------------------------------------
-$1
-------------------------------------
-}
-
-shell_message('tmux install script')
-
 cat << Output
 ------------------------------------
                  tmux install script
 ------------------------------------
 Output
 
-# if [[ "$(uname)" == "Darwin" ]]; then
+if [[ "$(uname)" == "Darwin" ]]; then
 
-#   # tmux installation
-# cat << Output
-#                                     ------------------------------------
-#                                     installing tmux
-#                                     ------------------------------------
-# Output
-#   brew install tmux
+  # tmux installation
+cat << Output
+                                    ------------------------------------
+                                    installing tmux
+                                    ------------------------------------
+Output
+  # brew install tmux
 
-# cat << Output
+cat << Output
+------------------------------------
+                               done!
+------------------------------------
+Output
 
-# ------------------------------------
-#                                done!
-# ------------------------------------
-# Output
+  # tmux plugin manager setup
+cat << Output
+------------------------------------
+      setting up tmux plugin manager
+------------------------------------
+Output
 
-#   # tmux plugin manager setup
-# cat << Output
-# ------------------------------------
-#       setting up tmux plugin manager
-# ------------------------------------
-# Output
+  # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-#   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# cat << Output
+cat << Output
+------------------------------------
+                               done!
+------------------------------------
+Output
 
-# ------------------------------------
-#                                done!
-# ------------------------------------
-# Output
+  # ReattachToUserNamespace installation
+cat << Output
+                                    ------------------------------------
+                                    installing ReattachToUserNamespace
+                                    ------------------------------------
+Output
+  # brew install reattach-to-user-namespace
 
-#   # ReattachToUserNamespace installation
-# cat << Output
-#                                     ------------------------------------
-#                                     installing ReattachToUserNamespace
-#                                     ------------------------------------
-# Output
-#   brew install reattach-to-user-namespace
+cat << Output
+------------------------------------
+                               done!
+------------------------------------
+Output
 
-# cat << Output
-
-# ------------------------------------
-#                                done!
-# ------------------------------------
-# Output
-
-# fi
+fi

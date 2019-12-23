59ed5b8f22367b0902dc714712b1b172ae4cc742
Hamid <hamidmamadzade@gmail.com>
2019-12-23 03:59:08 +0100

    setup script tweak

--- setup.sh
+++ setup.sh
@@ -1,59 +1,43 @@
 #!/bin/sh
 
-echo 'installing vim'
-
 if [[ "$(uname)" == "Darwin" ]]; then
+  # vim installation (mac comes pre-installed with vim but it is compiled without clipboard)
+  # this step is not needed if you have a version of vim that has system clipboard functionality
+  # you can check this by running 'vim --version | grep clipboard' in your terminal
 
-cat << VimOutput
-  vim installation (mac comes pre-installed with vim but it is compiled without clipboard)     |
-  this step is not needed if you have a version of vim that has system clipboard functionality |
-  you can check this by running 'vim --version | grep clipboard' in your terminal              |
-
-  'vim --version | grep clipboard'
-`vim --version | grep clipboard`
-
-  if you see '+clipboard' then you're fine
-  if you see '-clipboard' then run
-
-  'brew update && brew install vim'
-  '...everything will will install the first time you start vim'
-VimOutput
+  echo 'checking/setting up vim (see notes in script)'
+  [[ $(vim --version | grep clipboard) == *"-clipboard"* ]] && brew update && brew install vim
+  ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+  echo '...everything will will install the first time you start vim'
+  echo '. . . . . . . . . Done!'
 
   echo 'installing ctags'
   brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+  ln -s ~/dotfiles/ctags ~/.ctags
   echo '. . . . . . . . . Done!'
 
-  echo 'installing tmux'
-  brew install tmux
-  echo '. . . . . . . . . Done!'
-
-  echo 'installing ripgrep'
-  brew install ripgrep
-  echo '. . . . . . . . . Done!'
-
-  echo 'installing fzf'
-  brew install fzf
+  echo 'installing tmux, ripgrep and fzf'
+  brew install tmuxinator tmux ripgrep fzf
   $(brew --prefix)/opt/fzf/install
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
   echo '. . . . . . . . . Done!'
 
   echo 'installing asdf'
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-  cd ~/.asdf
-  git checkout "$(git describe --abbrev=0 --tags)"
-  cd $HOME
+  cd ~/.asdf; git checkout "$(git describe --abbrev=0 --tags)"
   echo '. . . . . . . . . Done!'
-
 fi
 
 echo 'installing terminal colours'
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 echo '. . . . . . . . . Done!'
 
-ln -s $HOME/dotfiles/vim/vimrc .vimrc
-ln -s $HOME/dotfiles/tmux/tmux.conf .tmux.conf
-ln -s $HOME/dotfiles/ctags .ctags
+echo 'changing default shell to zsh'
+[[ $SHELL != *"zsh" ]] && chsh -s /bin/zsh
+ln -s ~/dotfiles/zsh/vanilla-zshrc ~/.zshrc
+echo '. . . . . . . . . Done!'
 
-echo 'changing default shell'
-ln -s $HOME/dotfiles/zsh/vanilla-zshrc .zshrc
-chsh -s /bin/zsh
+echo 'kitty setup'
+ln -sf ~/dotfiles/config/kitty ~/.config/
 echo '. . . . . . . . . Done!'

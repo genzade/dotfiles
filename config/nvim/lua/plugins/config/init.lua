299802f9cb2b7b34b8f861aed23ff9b20f3f2851
Hamid Zadeh <hamidmamadzade@gmail.com>
2021-05-31 22:18:20 +0100

    adds nvim plugin config init file

---
+++ config/nvim/lua/plugins/config/init.lua
@@ -0,0 +1,13 @@
+return {
+  setup = function(use)
+    require'plugins.config.barbar'.setup(use)
+    require'plugins.config.base16'.setup(use)
+    require'plugins.config.fterm'.setup(use)
+    require'plugins.config.gitsigns'.setup(use)
+    require'plugins.config.lualine'.setup(use)
+    require'plugins.config.nvim-tree'.setup(use)
+    require'plugins.config.overlength'.setup(use)
+    require'plugins.config.telescope'.setup(use)
+    require'plugins.config.treesitter'.setup(use)
+  end
+}

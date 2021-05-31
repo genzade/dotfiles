4153c1c7cb601a4bae62e6e1bb7822faff0459b7
Hamid Zadeh <hamidmamadzade@gmail.com>
2021-05-31 23:23:17 +0100

    adds lsp plugins to new format (lspconfig and lspinstall)

---
+++ config/nvim/lua/plugins/config/lspconfig.lua
@@ -0,0 +1,9 @@
+-- local function config()
+-- end
+
+return {
+  setup = function(use)
+    -- Config for builtin LSP client
+    use "neovim/nvim-lspconfig"
+  end
+}
---
+++ config/nvim/lua/plugins/config/lspinstall.lua
@@ -0,0 +1,9 @@
+-- local function config()
+-- end
+
+return {
+  setup = function(use)
+    -- Language servers installer
+    use "kabouzeid/nvim-lspinstall"
+  end
+}

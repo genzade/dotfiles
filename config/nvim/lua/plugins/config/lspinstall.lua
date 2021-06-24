92e39124de48e66ba67ea970a8e0f1e4f8f5998c
Hamid Zadeh <hamidmamadzade@gmail.com>
2021-06-24 21:49:43 +0100

    `nvim` adds lspinstall to refactored plugin config

--- config/nvim/lua/plugins/config/lspinstall.lua
+++ config/nvim/lua/plugins/config/lspinstall.lua
@@ -1,9 +1,11 @@
 -- local function config()
 -- end
-
 return {
   setup = function(use)
     -- Language servers installer
-    use "kabouzeid/nvim-lspinstall"
-  end
+    use {
+      "kabouzeid/nvim-lspinstall",
+      -- config = config
+    }
+  end,
 }

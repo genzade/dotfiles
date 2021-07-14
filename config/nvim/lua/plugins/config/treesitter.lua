03366be7b706decaaabc2fae6335a50c4a79444f
Hamid Zadeh <hamidmamadzade@gmail.com>
2021-07-14 22:48:30 +0100

    ensure installed all maintained `nvim` treesitter langs

--- config/nvim/lua/plugins/config/treesitter.lua
+++ config/nvim/lua/plugins/config/treesitter.lua
@@ -2,7 +2,7 @@ local function config()
   -- Treesitter config
   require"nvim-treesitter.configs".setup {
     -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    ensure_installed = { "bash", "lua", "ruby" },
+    ensure_installed = "maintained",
     highlight = { enable = true },
   }
 end

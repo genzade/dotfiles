95ffed0939082b2c88afd9753d6e4ac8a510144a
Hamid Zadeh <hamidmamadzade@gmail.com>
2021-10-09 15:43:35 +0100

    nvim tree config tweak

--- config/nvim/lua/plugins/config/nvim-tree.lua
+++ config/nvim/lua/plugins/config/nvim-tree.lua
@@ -4,10 +4,12 @@ local function config()
     local nvim_tree = require "nvim-tree"
 
     local open_current_file_in_tree = function()
+      require"bufferline.state".set_offset(31, "FileTree")
       nvim_tree.find_file(true)
     end
 
     local close_nvim_tree = function()
+      require"bufferline.state".set_offset(0)
       nvim_tree.close()
     end
 
@@ -24,6 +26,21 @@ local function config()
   local opts = { noremap = true, silent = true }
 
   map("n", "<Leader>e", "<CMD>lua NvimTreeSmartToggle()<CR>", opts)
+
+  vim.g.nvim_tree_quit_on_open = 0
+  vim.g.nvim_tree_icons = {
+    default = '',
+    symlink = '',
+    git = {
+      unstaged = "",
+      staged = "✓",
+      unmerged = "",
+      renamed = "➜",
+      untracked = "",
+    },
+  }
+
+  vim.cmd("autocmd Colorscheme * highlight NvimTreeNormal guibg=#21252B guifg=#9da5b3")
 
   -- function NvimTreeXdgOpen()
   --   local lib = require "nvim-tree.lib"

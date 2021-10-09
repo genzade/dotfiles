2e933b36af81938dfca15ae88f783f0e12895249
Hamid Zadeh <hamidmamadzade@gmail.com>
2021-10-09 15:33:53 +0100

    nvim base16 gardening

--- config/nvim/lua/plugins/config/base16.lua
+++ config/nvim/lua/plugins/config/base16.lua
@@ -1,13 +1,13 @@
 local function config()
   -- Base16 config
-  vim.cmd "colorscheme base16-default-dark"
-
   if vim.fn.filereadable("~/.vimrc_background") then
     vim.g.base16colorspace = 256
     vim.cmd("source ~/.vimrc_background")
     vim.cmd("hi LineNr guibg=none")
     vim.cmd("hi CursorLineNr guibg=none guifg=#98c379 gui=bold")
     vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+  else
+    vim.cmd("colorscheme base16-default-dark")
   end
 end
 

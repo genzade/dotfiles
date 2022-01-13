a640f902c706d3fd01234c1656b0c55dc62d47db
Hamid Zadeh <hamidmamadzade@gmail.com>
2022-01-14 00:56:11 +0100

    nvim: null-ls, add shfmt + gardening

--- config/nvim/lua/plugins/config/null-ls.lua
+++ config/nvim/lua/plugins/config/null-ls.lua
@@ -4,21 +4,37 @@ local function config()
     return
   end
   local formatting = null_ls.builtins.formatting
-  local luaformatter_config = HOME .. "/dotfiles/lua/luaformatter/config.yml"
+  local lua_fmt_file = HOME .. "/dotfiles/lua/luaformatter/config.yml"
   local on_attach = require"plugins.config.lsp.defaults".on_attach
 
-  null_ls.setup({
-    sources = {
-      formatting.lua_format.with({
-        extra_args = { "--in-place", "--config=" .. luaformatter_config },
-      }),
-      formatting.prettier.with({
-        extra_args = { "--quote-props preserve", "--parser json5" },
-      }),
-      formatting.rubocop,
-    },
-    on_attach = on_attach,
-  })
+  null_ls.setup(
+    {
+      sources = {
+        formatting.lua_format.with(
+          { extra_args = { "--in-place", "--config=" .. lua_fmt_file } }
+        ),
+        formatting.prettier.with(
+          {
+            extra_args = { "--quote-props preserve", "--parser json5" },
+          }
+        ),
+        formatting.rubocop,
+        formatting.shfmt.with(
+          {
+            extra_args = {
+              "-i=2", -- Indent: 0 for tabs (default), >0 for number of spaces.
+              "-bn", -- binary ops like && and | may start a line
+              "-ci", -- switch cases will be indented
+              "-s",
+              "-sr", -- keep column alignment paddings
+              "-kp", -- function opening braces are placed on a separate line
+            },
+          }
+        ),
+      },
+      on_attach = on_attach,
+    }
+  )
 end
 
 return {

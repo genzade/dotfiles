3f534c093322fdcfca2256bcd1fc1a18e0c27005
Hamid Zadeh <hamidmamadzade@gmail.com>
2022-01-17 00:23:56 +0100

    nvim: adds `cmp-emoji` to completion

--- config/nvim/lua/plugins/config/completion.lua
+++ config/nvim/lua/plugins/config/completion.lua
@@ -5,8 +5,9 @@ local function has_words_before()
 
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and
-           vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col)
-             :match("%s") == nil
+           vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(
+             col, col
+           ):match("%s") == nil
 end
 
 local function config()
@@ -32,10 +33,12 @@ local function config()
       ["<C-d>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-e>"] = cmp.mapping.close(),
-      ["<C-y>"] = cmp.mapping(cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = true,
-      }, { "i", "c" }),
+      ["<C-y>"] = cmp.mapping(
+        cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Insert,
+          select = true,
+        }, { "i", "c" }
+      ),
 
       ["<C-space>"] = cmp.mapping {
         i = cmp.mapping.complete(),
@@ -56,27 +59,31 @@ local function config()
         select = true,
       },
 
-      ["<A-j>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.select_prev_item()
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
+      ["<A-j>"] = cmp.mapping(
+        function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }
+      ),
 
-      ["<A-k>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
+      ["<A-k>"] = cmp.mapping(
+        function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }
+      ),
     },
 
     sources = {
@@ -86,6 +93,7 @@ local function config()
       { name = "path" },
       { name = "luasnip" },
       { name = "buffer", keyword_length = 5 },
+      { name = "emoji" },
     },
 
     sorting = {
@@ -98,8 +106,10 @@ local function config()
         -- copied from cmp-under, but I don't think I need the plugin for this.
         -- I might add some more of my own.
         function(entry1, entry2)
-          local _, entry1_under = entry1.completion_item.label:find "^_+"
-          local _, entry2_under = entry2.completion_item.label:find "^_+"
+          local _, entry1_under =
+            entry1.completion_item.label:find "^_+"
+          local _, entry2_under =
+            entry2.completion_item.label:find "^_+"
           entry1_under = entry1_under or 0
           entry2_under = entry2_under or 0
           if entry1_under > entry2_under then
@@ -129,6 +139,7 @@ local function config()
         with_text = true,
         menu = {
           nvim_lsp = "[LSP]",
+          emoji = "[EMOJ]",
           luasnip = "[SNIP]",
           nvim_lua = "[API]",
           path = "[PATH]",
@@ -151,17 +162,25 @@ local function config()
     },
   }
 
-  cmp.setup.cmdline("/", {
-    sources = cmp.config.sources({ { name = "nvim_lsp_document_symbol" } },
-      { { name = "buffer" } }),
-  })
+  cmp.setup.cmdline(
+    "/", {
+      sources = cmp.config.sources(
+        { { name = "nvim_lsp_document_symbol" } },
+        { { name = "buffer" } }
+      ),
+    }
+  )
 
-  cmp.setup.cmdline(":", {
-    sources = cmp.config.sources({ { name = "path" } }, {
-      -- increase keyword_length if it gets annoying
-      { name = "cmdline", max_item_count = 30, keyword_length = 1 },
-    }),
-  })
+  cmp.setup.cmdline(
+    ":", {
+      sources = cmp.config.sources(
+        { { name = "path" } }, {
+          -- increase keyword_length if it gets annoying
+          { name = "cmdline", max_item_count = 30, keyword_length = 1 },
+        }
+      ),
+    }
+  )
 
   -- shout out to Telescopic Johnson
 end
@@ -176,6 +195,7 @@ return {
     use { "hrsh7th/cmp-nvim-lua" }
     use { "hrsh7th/cmp-nvim-lsp" }
     use { "hrsh7th/cmp-buffer" }
+    use { "hrsh7th/cmp-emoji" }
 
     use { "onsails/lspkind-nvim" }
   end,

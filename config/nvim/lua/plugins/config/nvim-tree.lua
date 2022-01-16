36ad282c07a86a38a28f80aadc2c45c346a37c13
Hamid Zadeh <hamidmamadzade@gmail.com>
2022-01-17 00:31:57 +0100

    nvim: fix nvim-tree bg colours

--- config/nvim/lua/plugins/config/nvim-tree.lua
+++ config/nvim/lua/plugins/config/nvim-tree.lua
@@ -31,12 +31,16 @@ function NvimTreeSmartToggle()
 end
 
 local function config()
-  vim.api.nvim_exec([[
-    augroup NVIMTREE
-      autocmd!
-      autocmd Colorscheme * highlight NvimTreeNormal guibg=#21252B guifg=#9da5b3
-    augroup END
-  ]], true)
+  vim.api.nvim_exec(
+    [[
+      augroup NVIMTREE
+        autocmd!
+        autocmd Colorscheme * highlight! NvimTreeNormal guibg=#21252B guifg=#9da5b3
+        autocmd ColorScheme * highlight! NvimTreeBg guibg=#2B4252
+        autocmd FileType NvimTree setlocal winhighlight=Normal:NvimTreeBg
+      augroup END
+    ]], false
+  )
 
   local map = vim.api.nvim_set_keymap
   local opts = { noremap = true, silent = true }
@@ -65,10 +69,16 @@ local function config()
       symlink = "",
       symlink_open = "",
     },
-    lsp = { hint = "", info = "", error = "", warning = "" },
+    lsp = { hint = "", info = "", error = "",
+            warning = "" },
   }
 
-  require"nvim-tree".setup { disable_netrw = true, hijack_netrw = true }
+  local nvim_tree_status_ok, nvim_tree = pcall(require, "nvim-tree")
+  if not nvim_tree_status_ok then
+    return
+  end
+
+  nvim_tree.setup { disable_netrw = true, hijack_netrw = true }
 end
 
 return {

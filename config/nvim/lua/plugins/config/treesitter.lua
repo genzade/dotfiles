ec259e8f996748b4396fce9adef670b0295553f5
Hamid Zadeh <hamidmamadzade@gmail.com>
2022-01-17 00:42:13 +0100

    nvim: add indent option to treesitter + gardening

--- config/nvim/lua/plugins/config/treesitter.lua
+++ config/nvim/lua/plugins/config/treesitter.lua
@@ -1,15 +1,23 @@
 local function config()
-  -- Treesitter config
-  require"nvim-treesitter.configs".setup {
+  local has_ts_config, ts_config = pcall("nvim-treesitter.configs")
+  if not has_ts_config then
+    return
+  end
+
+  ts_config.setup {
     -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     ensure_installed = "maintained",
     highlight = { enable = true },
+    indent = { enable = true },
   }
 end
 
 return {
   setup = function(use)
-    -- Treesitter language parser generator tool
-    use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate", config = config }
+    use {
+      "nvim-treesitter/nvim-treesitter",
+      run = ":TSUpdate",
+      config = config,
+    }
   end,
 }

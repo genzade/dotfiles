a6a375bcd5f62eb462415186d3155c008a8580f7
Hamid Zadeh <hamidmamadzade@gmail.com>
2022-01-17 00:26:10 +0100

    nvim: add a few new language servers to lspconfig

--- config/nvim/lua/plugins/config/lspconfig.lua
+++ config/nvim/lua/plugins/config/lspconfig.lua
@@ -1,77 +1,101 @@
 local function lsp_installer_config()
-  local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+  local signs = {
+    Error = " ",
+    Warn = " ",
+    Hint = " ",
+    Info = " ",
+  }
   for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
   end
 
   local servers = {
+    "bashls",
     "clangd",
+    "cssls",
+    "dockerls",
     "html",
     "solargraph",
     "sumneko_lua",
     "tailwindcss",
     "tsserver",
+    "yamlls",
   }
 
-  local lsp_installer_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
-  if not lsp_installer_ok then
+  local has_installer, lsp_installer =
+    pcall(require, "nvim-lsp-installer")
+  if not has_installer then
     return
   end
 
-  lsp_installer.settings({
-    ui = {
-      icons = {
-        server_installed = "✓",
-        server_pending = "➜",
-        server_uninstalled = "✗",
+  lsp_installer.settings(
+    {
+      ui = {
+        icons = {
+          server_installed = "✓",
+          server_pending = "➜",
+          server_uninstalled = "✗",
+        },
       },
-    },
-  })
+    }
+  )
 
   local function make_config()
     local on_attach = require"plugins.config.lsp.defaults".on_attach
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+    capabilities = require("cmp_nvim_lsp").update_capabilities(
+                     capabilities
+                   )
 
     return {
       capabilities = capabilities,
       on_attach = on_attach,
-      flags = { debounce_text_changes = 150 },
+      flags = { debounce_text_changes = 300 },
     }
   end
 
-  local runtime_path = vim.split(package.path, ";")
-  table.insert(runtime_path, "lua/?.lua")
-  table.insert(runtime_path, "lua/?/init.lua")
+  local enhance_server_opts = {
+    ["sumneko_lua"] = function(opts)
+      local runtime_path = vim.split(package.path, ";")
 
-  local lua_settings = {
-    Lua = {
-      runtime = { version = "LuaJIT", path = runtime_path },
-      diagnostics = { globals = { "vim" } },
-      workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-      telemetry = { enable = false },
-    },
+      table.insert(runtime_path, "lua/?.lua")
+      table.insert(runtime_path, "lua/?/init.lua")
+
+      opts.settings = {
+        Lua = {
+          runtime = { version = "LuaJIT", path = runtime_path },
+          diagnostics = { globals = { "vim" } },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          telemetry = { enable = false },
+        },
+      }
+    end,
   }
 
   for _, name in pairs(servers) do
     local server_is_found, server = lsp_installer.get_server(name)
     if server_is_found then
-      server:on_ready(function()
-        local config = make_config()
+      server:on_ready(
+        function()
+          local opts = make_config()
 
-        if server.name == "sumneko_lua" then
-          config.settings = lua_settings
+          if enhance_server_opts[server.name] then
+            -- Enhance the default opts with the server-specific ones
+            enhance_server_opts[server.name](opts)
+          end
+
+          if server.name == "solargraph" then
+            -- not working with nvim lsp installer, have to use lspconfig for this
+            -- investigate/debug and then open an issue
+            return
+          end
+
+          server:setup(opts)
         end
-
-        if server.name == "solargraph" then
-          -- not working with nvim lsp installer, have to use lspconfig for this
-          -- investigate/debug and then open an issue
-          return
-        end
-
-        server:setup(config)
-      end)
+      )
 
       if not server:is_installed() then
         print("Installing " .. name)
@@ -80,7 +104,12 @@ local function lsp_installer_config()
     end
   end
 
-  require"lspconfig".solargraph.setup(make_config())
+  local has_lspconfig, lspconfig = pcall(require, "lspconfig")
+  if not has_lspconfig then
+    return
+  end
+
+  lspconfig.solargraph.setup(make_config())
 end
 
 return {

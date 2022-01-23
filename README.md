92780a6d4d0ab9cc8b3ca8d5af077e63e3dd7c2a
Hamid Zadeh <hamidmamadzade@gmail.com>
2022-01-24 00:05:01 +0100

    Update `install` script and update `README`

--- README.md
+++ README.md
@@ -1,80 +1,16 @@
-# I Didn't Choose The DOTLife, THE DOTLife CHOSE ME!
+# *dotfiles* 
 
-This is my basic dotfile, I like to use vim with tmux in a z shell specifically
-oh-my-zsh.
+Repository containing all the automation and dotfiles required to setup my MacOS machine after a fresh install.
 
-## SETUP
+- nvim (vim8 also available)
+  - fully configured in lua
+  - builtin lsp
+  - completion with snippets
+  - treesitter
+  - and more
+- tmux
+  - tpm
 
-###...IT'S A BEAUTIFUL DOTLife
+See `install.sh` to get an overview of the installation of my current setup on `MacOS`.
 
-Make sure your terminal is lookin' good. This [insert link to my powerline
-setup] or check [this general guide](http://powerline.readthedocs.io/en/master/installation/osx.html)
-
-## CONFIG TIPS AND TRICKS
-
-Installing [chriskempson's base16 project]( https://github.com/chriskempson/base16 ) means you can change the terminal
-color scheme with considerable ease from the command line.
-
-Just choose a base16 theme and type in your terminal;
-
-```
-$ base16_<coloersheme_of_your_choice>
-```
-
-This will set your $BASE16_THEME to the coloersheme_of_your_choice chosen from
-the base16 project.
-
-## useful vim tips and tricks
-
-### search and replace
-
-Let's say you have some variable or keyword `foo` all over your project and you
-decide you wanted to change `foo` to `bar`. You don't, however, want to open
-every file that has `foo` and manually change it to `bar` mainly because you're
-not a loser and I believe in you!
-
-Simple - combine vim's args function with mileszs' 'ack' plugin to perfection.
-Have a look at the following instructions...
-
-```
-:args `ag -l foo .`
-```
-
-Here we use `ag` to find occurrences of the word `foo` in our working directory
-(note that we use the `-l` flag to list the files) and pass it into vim's
-args.
-
-```
-:argdo %s/foo/bar/g | w
-```
-
-We then use vim's argdo function to do a global (`/g`) search (`%s`) and replace
-as well as saving (writing) each file with `| w` and finally visit youtube to
-research what the best microphone available to purchase on the market, make
-the order and upon delivery of said microphone, drop that bad boy because your
-search-and-replace-headache-having-days are done.
-
-### function vimfix
-
-Lets say you are working on a project with in version control and you have a
-merge conflict. Of course you would first type;
-
-```
-$ git status
-```
-
-in order to see which files are affected and thus which files need some special
-attention. Then you would make the necessary changes in your favourite editor
-whatever that may be (I know for some of you it's not vim, it's okay, I'm not
-judging you. I just think you need to reevaluate some of the decisions you make
-in your life).
-
-Well this is where I found `vimfix` to be something of a game-changer because
-now if there is a merge conflict I can just type
-
-```
-$ vimfix
-```
-
-which will open up all files with merge conflicts with vim in separate tabs.
-
+### TODO
--- install.sh
+++ install.sh
@@ -1,137 +1,212 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-cat << Output
-------------------------------------------------------------------------
-                                                     INSTALLATION-SCRIPT
-------------------------------------------------------------------------
+set -e
 
+DOTFILES_ROOT="$HOME/dotfiles"
 
+setup_osx() {
+  if [ "$(uname -s)" == "Darwin" ]; then
+    echo -e "\n\nRunning on OSX"
 
-------------------------------------
-                             install
-------------------------------------
-Output
+    "$DOTFILES_ROOT"/mac/osx-defaults.sh
+  fi
+}
 
-if [[ "$(uname)" == "Darwin" ]]; then
+setup_homebrew() {
+  which brew \
+    || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-  # fuzzy finder setup
-cat << Output
-------------------------------------
-                      installing fzf
-------------------------------------
-Output
+  brew update
+  brew cleanup
+  brew bundle install --file="$DOTFILES_ROOT/mac/Brewfile"
+}
 
-  brew install fzf
+setup_karabiner() {
+  KARABINER_CONFIG_DIR="$HOME/.config/karabiner"
 
-cat << Output
-                                    ------------------------------------
-                                    installing fzf fuzzy completion
-                                    ------------------------------------
-Output
+  if [ -L "$KARABINER_CONFIG_DIR" ]; then
+    if [ -d "$KARABINER_CONFIG_DIR" ]; then
+      echo "symlink exists: NOT linking to $KARABINER_CONFIG_DIR"
+      echo "symlink exists: NOT linking to $KARABINER_CONFIG_DIR"
+    fi
+  else
+    ln -s "$DOTFILES_ROOT"/config/karabiner "$HOME"/.config
+    echo "linked $DOTFILES_ROOT/config/karabiner to $KARABINER_CONFIG_DIR"
+    launchctl kickstart -k gui/"$(id -u)"/org.pqrs.karabiner.karabiner_console_user_server
+  fi
+}
 
-  # To install useful key bindings and fuzzy completion:
-  $(brew --prefix)/opt/fzf/install
+setup_git() {
+  GITCONFIG=$HOME/.gitconfig
+  GITIGNORE_GLOBAL=$HOME/.gitignore_global
 
-  # asdf, the extendable version manager.
-  # Supported languages include Ruby, Node.js, Elixir and more.
-cat << Output
-------------------------------------
-                     installing asdf
-------------------------------------
-Output
+  if ! [ -L "$GITCONFIG" ]; then
+    echo "Creating symlink $GITCONFIG."
+    ln -sf "$DOTFILES_ROOT"/git/gitconfig "$GITCONFIG"
+  else
+    echo "symlink exists: $GITCONFIG."
+  fi
 
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.4.0
-  echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc
-  echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
+  if ! [ -L "$GITIGNORE_GLOBAL" ]; then
+    echo "Creating symlink $GITIGNORE_GLOBAL."
+    ln -sf "$DOTFILES_ROOT"/git/gitignore_global "$GITIGNORE_GLOBAL"
+  else
+    echo "symlink exists: $GITIGNORE_GLOBAL."
+  fi
+}
 
-  # tig: text-mode interface for Git
-cat << Output
-                                    ------------------------------------
-                                    installing tig
-                                    ------------------------------------
-Output
+setup_zsh() {
+  [[ $SHELL != *"zsh" ]] && chsh -s /bin/zsh
 
-  brew install tig
+  ZSHRC=$HOME/.zshrc
+  if ! [ -L "$ZSHRC" ]; then
+    echo "Creating symlink $ZSHRC."
+    ln -s "$DOTFILES_ROOT"/zsh/zshrc "$ZSHRC"
+  else
+    echo "symlink exists: $ZSHRC."
+  fi
+}
 
-  # openssl: general purpose cryptographic library
-cat << Output
-------------------------------------
-                  installing openssl
-------------------------------------
-Output
+setup_alacritty() {
+  # the app itself should have been installed with the Brewfile.
+  # setup will make sure terminfo is installed and config is symlinked
 
-  brew install openssl
+  ALACRITTY_CONFIG_DIR=$HOME/.config/alacritty
 
-  # Programming language prerequisites and package managers
-cat << Output
-                                    ------------------------------------
-                                    installing libyaml
-                                    ------------------------------------
-Output
+  if ! (toe | grep -q alacritty); then
+    echo "terminfo not installed"
 
-  brew install libyaml # should come after openssl
+    ALACRITTY_CLONE=$HOME/alacritty
 
-  # basic file, shell and text manipulation utilities of the GNU operating system
-cat << Output
-------------------------------------
-                installing coreutils
-------------------------------------
-Output
+    # clone
+    git clone https://github.com/alacritty/alacritty.git "$ALACRITTY_CLONE"
+    cd "$ALACRITTY_CLONE"
 
-  brew install coreutils
+    sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
 
-  # yarn: a new package manager for javaScript
-cat << Output
-                                    ------------------------------------
-                                    installing yarn
-                                    ------------------------------------
-Output
+    cd ..
+    rm -rf "$ALACRITTY_CLONE"
+  else
+    echo "alacritty terminfo already added"
+  fi
 
-  brew install yarn
+  if [ -L "$ALACRITTY_CONFIG_DIR" ]; then
+    if [ -d "$ALACRITTY_CONFIG_DIR" ]; then
+      echo  "symlink exists: NOT linking to $ALACRITTY_CONFIG_DIR"
+    fi
+  else
+    ln -s "$DOTFILES_ROOT"/config/alacritty "$ALACRITTY_CONFIG_DIR"
+    echo "linked $DOTFILES_ROOT/config/alacritty to $ALACRITTY_CONFIG_DIR"
+  fi
 
-  # create, edit, compose, or convert bitmap images
-cat << Output
-------------------------------------
-              installing ImageMagick
-------------------------------------
-Output
+  if ! [ -d "${ZDOTDIR:-~}"/.zsh_functions ]; then
+    echo "${ZDOTDIR:-~}/.zsh_functions does not exist, creating now"
+    mkdir -p "${ZDOTDIR:-~}"/.zsh_functions
+  fi
+}
 
-  brew install imagemagick
+setup_fzf() {
+  # should be installed already, see Brewfile
+  if [ -f '/usr/local/opt/fzf/install' ]; then
+    echo "finish setting up fzf for zsh"
 
-  # Use GPG Suite to encrypt, decrypt, sign and verify files or messages
-cat << Output
-                                    ------------------------------------
-                                    installing GNUpg
-                                    ------------------------------------
-Output
+    /usr/local/opt/fzf/install \
+      --all \
+      --no-update-rc \
+      --completion \
+      --key-bindings \
+      --no-fish \
+      --no-bash
+  else
+    echo "WARNING: fzf install command not found. Skipping."
+  fi
+}
 
-  brew install gpg
+setup_asdf() {
+  ASDF_DIR=$HOME/.asdf
+  if ! [ -d "$ASDF_DIR" ]; then
+    # will use git to install asdf instead of homebrew
+    # see https://github.com/asdf-vm/asdf/issues/785
+    echo "installing asdf plugin manager in $ASDF_DIR"
 
-  # MySQL database
-cat << Output
-------------------------------------
-                    installing MySQL
-------------------------------------
-Output
+    git clone https://github.com/asdf-vm/asdf.git "$ASDF_DIR"
+    cd "$ASDF_DIR"
+    git checkout "$(git describe --abbrev=0 --tags)"
 
-  brew install mysql
+    cd ..
+  else
+    echo "asdf already installed"
+  fi
 
-  # A code searching tool.
-cat << Output
-                                    ------------------------------------
-                                    installing ripgrep
-                                    ------------------------------------
-Output
+  GLOBAL_TOOL_VERSIONS="$HOME"/.tool-versions
 
-  brew install ripgrep
+  if ! [ -L "$GLOBAL_TOOL_VERSIONS" ]; then
+    echo "Creating symlink $GLOBAL_TOOL_VERSIONS."
+    ln -sf "$DOTFILES_ROOT"/asdf/tool-versions "$GLOBAL_TOOL_VERSIONS"
+  else
+    echo "symlink exists: $GLOBAL_TOOL_VERSIONS."
+  fi
 
-  # colorscheme setup
-cat << Output
-------------------------------------
-                              base16
-------------------------------------
-Output
+  if [ -f "$HOME"/.tool-versions ]; then
+    while IFS=' ' read -ra tools; do
+      PLUGIN=${tools[0]}
+      VERSION=${tools[1]}
 
-  git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+      if  ! asdf plugin-list | grep -q "$PLUGIN"; then
+        echo     "plugin $PLUGIN does not exist, installing now..."
 
-fi
+        asdf     plugin-add "$PLUGIN"
+        asdf     plugin update "$PLUGIN"
+        asdf     install "$PLUGIN" "$VERSION"
+      else
+        echo     "plugin $PLUGIN already setup"
+      fi
+    done < "$HOME"/.tool-versions
+  fi
+}
+
+setup_base16() {
+  BASE16_PATH="$HOME"/.config/base16-shell
+
+  if ! [ -d "$BASE16_PATH" ]; then
+    git clone https://github.com/chriskempson/base16-shell.git "$BASE16_PATH"
+  else
+    echo "already installed"
+  fi
+}
+
+setup_neovim() {
+  NEOVIM_PATH="$HOME/.config/nvim"
+
+  if [ -L "$NEOVIM_PATH" ]; then
+    if [ -d "$NEOVIM_PATH" ]; then
+      echo  "symlink exists: NOT linking to $NEOVIM_PATH"
+    fi
+  else
+    ln -s "$DOTFILES_ROOT"/config/nvim "$HOME"/.config
+    echo "linked $DOTFILES_ROOT/config/neovim to $NEOVIM_PATH"
+  fi
+}
+
+setup_tmux() {
+  TMUX_CONF=$HOME/.tmux.conf
+
+  if ! [ -L "$TMUX_CONF" ]; then
+    echo "Creating symlink $TMUX_CONF."
+    ln -s "$DOTFILES_ROOT"/tmux/tmux.conf "$TMUX_CONF"
+  else
+    echo "symlink exists: $TMUX_CONF."
+  fi
+}
+
+setup_homebrew
+setup_karabiner
+setup_git
+setup_zsh
+setup_alacritty
+setup_fzf
+setup_asdf
+setup_base16
+setup_neovim
+setup_tmux
+setup_osx

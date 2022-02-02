7a83872b238f0af1eba3d4f630278c67b56a7325
Hamid Zadeh <hamidmamadzade@gmail.com>
2022-02-02 23:27:33 +0100

    nvim: fix base16 signcolumn colours

--- config/nvim/lua/plugins/config/base16.lua
+++ config/nvim/lua/plugins/config/base16.lua
@@ -6,17 +6,18 @@ local function config()
 
   local Path = require("plenary.path")
   local vimrc_bg_file = Path:new({ HOME, ".vimrc_background" })
+  local cmd = vim.cmd
 
   if vimrc_bg_file:exists() then
-    vim.cmd([[source ]] .. vimrc_bg_file.filename)
+    cmd([[source ]] .. vimrc_bg_file.filename)
 
     fwatch.watch(
       vimrc_bg_file.filename, {
         on_event = vim.schedule_wrap(
           function(_, events, unwatch)
             if events.change then
-              vim.cmd([[source ]] .. vimrc_bg_file.filename)
-              vim.cmd([[PackerCompile]])
+              cmd([[source ]] .. vimrc_bg_file.filename)
+              cmd([[PackerCompile]])
             end
 
             unwatch()
@@ -25,24 +26,29 @@ local function config()
       }
     )
   else
-    vim.cmd([[colorscheme base16_default-dark]])
+    cmd([[colorscheme base16_default-dark]])
   end
 
-  vim.cmd([[hi LineNr guibg=none]])
-  vim.cmd([[hi CursorLineNr guibg=none guifg=#98c379 gui=bold]])
-  vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
+  cmd([[hi LineNr guibg=none]])
+  cmd([[hi CursorLineNr guibg=none guifg=#98c379 gui=bold]])
+  cmd([[hi Normal guibg=NONE ctermbg=NONE]])
 
   -- example of how to ovverride any colorus, below shows how to ovverride
   -- some highlight groups
   -- (see https://github.com/RRethy/nvim-base16/blob/b1b9fe2c91/lua/base16-colorscheme.lua)
-  -- local has_colors, colorscheme = pcall(require, "base16-colorscheme")
-  -- if not has_colors then
-  --   return
-  -- end
-  -- local hi = colorscheme.highlight
-  -- hi.StatusLineFileModified = { guibg = "#555965", guifg = "#00aa00" }
-  -- hi.StatusLineFileReadonly = { guibg = "#555965", guifg = "#ffa003" }
-  -- hi.StatusLineFileRestricted = { guibg = "#555965", guifg = "#dc322f" }
+  local has_colors, base16 = pcall(require, "base16-colorscheme")
+  if not has_colors then
+    return
+  end
+  local colors = base16.colors
+  local hi = base16.highlight
+
+  hi.SignColumn = {
+    guifg = colors.base04,
+    guibg = "none",
+    gui = nil,
+    guisp = nil,
+  }
 end
 
 return {

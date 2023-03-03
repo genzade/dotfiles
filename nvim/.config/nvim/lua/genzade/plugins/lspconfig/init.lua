271f6e4da73aa7b9339bda507622755574e88304
Hamid Zadeh <hamidmamadzade@gmail.com>
2023-03-03 14:50:36 +0000

    nvim: fix lsp lua warning on start up

--- nvim/.config/nvim/lua/genzade/plugins/lspconfig/init.lua
+++ nvim/.config/nvim/lua/genzade/plugins/lspconfig/init.lua
@@ -86,6 +86,7 @@ local config = function()
             diagnostics = { globals = { 'vim' } },
             workspace = {
               library = vim.api.nvim_get_runtime_file('', true),
+              checkThirdParty = false,
             },
             telemetry = { enable = false },
           },

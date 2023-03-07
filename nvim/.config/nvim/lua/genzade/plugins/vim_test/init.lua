29462935ec13e48a3436568bfc21de71eb09af56
Hamid Zadeh <hamidmamadzade@gmail.com>
2023-03-07 20:13:33 +0000

    nvim: add custom strategies to vim-test
    doubling down on vim-test as it is the most mature/best solution out
    there.
    - neotest, looks amazing but cannot install on macos freezes
    - nvim-test, lua cousin to vim-test but still missing some features like
      projectionist integration

--- nvim/.config/nvim/lua/genzade/plugins/vim_test/init.lua
+++ nvim/.config/nvim/lua/genzade/plugins/vim_test/init.lua
@@ -1,78 +1,51 @@
--- local config = function()
---   local which_key_ok, which_key = pcall(require, 'which-key')
---   if not which_key_ok then
---     vim.notify('which-key not found', vim.log.levels.ERROR)
---     return
---   end
---
---   local neotest_ok, neotest = pcall(require, 'neotest')
---   if not neotest_ok then
---     return
---   end
---
---   local neotest_plenary_ok, neotest_plenary = pcall(require, 'neotest-plenary')
---   if not neotest_plenary_ok then
---     return
---   end
---
---   local neotest_rspec_ok, neotest_rspec = pcall(require, 'neotest-rspec')
---   if not neotest_rspec_ok then
---     return
---   end
---
---   neotest.setup({
---     adapters = {
---       neotest_plenary,
---       neotest_rspec({
---         rspec_cmd = function()
---           return vim.tbl_flatten({
---             'bundle',
---             'exec',
---             'rspec',
---           })
---         end,
---       }),
---     },
---     -- icons = {
---     --   running_animated = { '⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷' },
---     --   passed = '✔',
---     --   running = '~',
---     --   failed = '✖',
---     --   skipped = 'ﰸ',
---     --   unknown = '?',
---     -- },
---   })
---
---   which_key.register({
---     ['<Leader>'] = {
---       t = {
---         name = '+TestRunner',
---         n = { neotest.run.run(), 'Run nearest test' },
---         f = { neotest.run.run(vim.fn.expand('%')), 'Run test file' },
---         -- s = { '<CMD>TestSuite<CR>', 'Run test suite' },
---         -- l = { neotest.run.run_last, 'Run last test' },
---         -- v = { '<CMD>TestVisit<CR>', 'Run test visit' },
---       },
---     },
---   }, { mode = 'n' })
--- end
---
--- return {
---   'nvim-neotest/neotest',
---   requires = {
---     'nvim-treesitter/nvim-treesitter',
---     'antoinemadec/FixCursorHold.nvim', -- remove once https://github.com/antoinemadec/FixCursorHold.nvim/issues/13 is fixed
---     'nvim-lua/plenary.nvim',
---     "nvim-neotest/neotest-plenary",
---     'olimorris/neotest-rspec',
---   },
---   config = config,
--- }
-
 local config = function()
+  local fterm_runner = function(cmd)
+    local ok, fterm = pcall(require, 'FTerm')
+    if not ok then
+      return
+    end
+
+    fterm.run('clear && ' .. cmd)
+  end
+
+  local tmux_runner = function(cmd)
+    local function sendCommand()
+      vim.fn.jobstart({ 'tmux', 'send-key', '-t', '.2', cmd, 'Enter' })
+    end
+
+    vim.fn.jobstart({ 'tmux', 'list-panes' }, {
+      stdout_buffered = true, -- get all stdout rather than line by line
+      on_stdout = function(_, data)
+        if #data >= 3 then -- at least 2 lines for pane info and 1 empty string for EOF
+          sendCommand()
+        else
+          vim.fn.jobstart({ 'tmux', 'split-window' }, { on_stdout = sendCommand })
+        end
+      end,
+    })
+  end
+
+  local select_strategy = function()
+    local set_selection = function(item, _)
+      vim.g['test#strategy'] = item
+    end
+
+    vim.ui.select({ 'tmux', 'fterm' }, { prompt = 'Select Test strategy:' }, set_selection)
+  end
+
+  vim.api.nvim_create_user_command('TestStrategySelect', select_strategy, { force = true })
+
+  vim.g['test#custom_strategies'] = {
+    fterm = fterm_runner,
+    tmux = tmux_runner,
+  }
+
+  vim.g['test#strategy'] = 'fterm'
+
   -- vim.g["test#strategy"] = "fterm"
   -- vim.g["test#strategy"] = "dispatch"
-  vim.g['test#strategy'] = 'neovim'
+  -- vim.g['test#strategy'] = 'neovim'
+  -- vim.g['test#preserve_screen'] = 1
 
   local which_key_ok, which_key = pcall(require, 'which-key')
   if not which_key_ok then
@@ -83,11 +56,12 @@ local config = function()
     ['<Leader>'] = {
       t = {
         name = '+TestRunner',
-        n = { '<CMD>TestNearest<CR>', 'Run nearest test' },
-        f = { '<CMD>TestFile<CR>', 'Run test file' },
-        s = { '<CMD>TestSuite<CR>', 'Run test suite' },
-        l = { '<CMD>TestLast<CR>', 'Run last test' },
-        v = { '<CMD>TestVisit<CR>', 'Run test visit' },
+        n = { '<CMD>TestNearest<CR>', 'Run [N]earest test' },
+        f = { '<CMD>TestFile<CR>', 'Run test [F]ile' },
+        s = { '<CMD>TestSuite<CR>', 'Run test [S]uite' },
+        S = { '<CMD>TestStrategySelect<CR>', 'Select test [S]trategy' },
+        l = { '<CMD>TestLast<CR>', 'Run [L]ast test' },
+        v = { '<CMD>TestVisit<CR>', 'Run test [V]isit' },
       },
     },
   }, { mode = 'n' })
@@ -95,5 +69,6 @@ end
 
 return {
   'vim-test/vim-test',
+  dependencies = { 'numtostr/FTerm.nvim', 'folke/which-key.nvim' },
   config = config,
 }

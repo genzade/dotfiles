a13b22a72203c9d46777939979192fcfbfa4c3cf
Hamid Zadeh <hamidmamadzade@gmail.com>
2023-03-23 10:48:36 +0000

    nvim: attempt at fixing markdown preview
    - still does not work, i had to manually run the build command

--- nvim/.config/nvim/lua/genzade/plugins/markdown_preview/init.lua
+++ nvim/.config/nvim/lua/genzade/plugins/markdown_preview/init.lua
@@ -1,12 +1,14 @@
 return {
   'iamcco/markdown-preview.nvim',
-  build = 'cd app && yarn install',
+  -- build = 'cd app && yarn install',
+  build = function()
+    vim.fn['mkdp#util#install']()
+  end,
   cmd = 'MarkdownPreview',
   config = function()
     vim.g.mkdp_filetypes = { 'markdown' }
   end,
   ft = { 'markdown' },
-
   -- "iamcco/markdown-preview.nvim",
   -- build = function()
   --   vim.fn["mkdp#util#install"]()

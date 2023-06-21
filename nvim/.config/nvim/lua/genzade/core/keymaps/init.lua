b1ad3940911880ed5df52fa85821514e25d4e3bf
Hamid Zadeh <hamidmamadzade@gmail.com>
2023-06-21 23:11:23 +0100

    nvim: minor refactor of keymaps

--- nvim/.config/nvim/lua/genzade/core/keymaps/init.lua
+++ nvim/.config/nvim/lua/genzade/core/keymaps/init.lua
@@ -16,15 +16,28 @@ map('v', ';', ':', { noremap = true })
 map('n', '<Leader><Space>', ':set hlsearch!<CR>')
 
 -- Tab navigation
-map('n', 'tn', ':tabnext<CR>')
-map('n', 'tp', ':tabprevious<CR>')
-map('n', 'tl', ':tablast<CR>')
-map('n', 'tf', ':tabfirst<CR>')
+map('n', 'tn', vim.cmd.tabnext)
+map('n', 'tp', vim.cmd.tabprevious)
+map('n', 'tl', vim.cmd.tablast)
+map('n', 'tf', vim.cmd.tabfirst)
+map('n', 'tx', vim.cmd.tabclose)
+map('n', 'to', vim.cmd.tabonly)
 map('n', 'tN', ':tabnew ', { noremap = true, silent = false })
 map('n', 'T', ':tabnew %<CR>')
 
--- copy filename to clipboard. TODO: refactor
-map('n', 'cp', ":let @*=expand('%')<CR>")
+-- something to consider
+-- gt    -   go to next tab
+-- gT    -   go to previous tab
+-- {i}gt -   go to tab in position i
+-- 1gt   -   go to first tab
+-- 1gT   -   go to last tab
+
+-- copy filename to clipboard
+map('n', 'cp', function()
+  local path = vim.fn.expand('%')
+  vim.fn.setreg('+', path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end)
 
 -- keep cursor position centred
 map('n', 'n', 'nzzzv')
